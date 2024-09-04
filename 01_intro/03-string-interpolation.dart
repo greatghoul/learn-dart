35c3c6e65d59b944a34836dc7be9db351503e6c1
greatghoul <greatghoul@gmail.com>
2024-09-04 07:43:52 +0000

    learn hello world and variables

---
+++ 01_intro/03-string-interpolation.dart
@@ -0,0 +1,5 @@
+void main() {
+  var firstName = "Mario";
+  var lastName = "Qiu";
+  print("Full name is $firstName $lastName");
+}
---
+++ 01_intro/05-variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Mario Qiu";
+  String address = "China";
+  int age = 38;
+  num height = 168.5;
+  bool isMarried = true;
+
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is ${height}cm");
+  print("Married Status is $isMarried");
+}
